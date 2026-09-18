7c192cd5b60800eb03ab5f7c3f8dec911b73ccc6
SathishMadesh <s747272946@gmail.com>
2026-09-18 20:52:12 +0530

    Temporarily run scheduler every minute

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,7 +41,7 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(
     collect_new_releases,
     "interval",
-    hours=1,
+    minutes=1,
     id="pib_collection_job",
     replace_existing=True
 )
